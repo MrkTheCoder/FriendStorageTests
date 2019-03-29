ec35755b533f8fd36f7e7d958ab1ebd0d5e6bedc
MRK <mrezak@gmail.com>
2019-03-29 12:07:09 +0430

    Add project files.

---
+++ FriendStorage.UITests/ViewModel/NavigationItemViewModelTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http.Headers;
+using FriendStorage.UI.Events;
+using FriendStorage.UI.ViewModel;
+using Moq;
+using Prism.Events;
+using Xunit;
+using OpenFriendEditViewEvent = FriendStorage.UI.Events.OpenFriendEditViewEvent;
+
+namespace FriendStorage.UITests.ViewModel
+{
+public class NavigationItemViewModelTest
+{
+    [Fact]
+    public void ShouldPublishOpenFriendEditViewEvent()
+    {
+        var friendId = 7;
+        var eventMoq = new Mock<OpenFriendEditViewEvent>();
+        var eventAggregatorMoq = new Mock<IEventAggregator>();
+        eventAggregatorMoq
+            .Setup(s =>s.GetEvent<OpenFriendEditViewEvent>())
+            .Returns(eventMoq.Object);
+
+        var viewModel = new NavigationItemViewModel(friendId, 
+            "Thomas", eventAggregatorMoq.Object);
+        viewModel.OpenFriendEditViewCommand.Execute(null);
+
+        eventMoq.Verify(of => of.Publish(friendId), Times.Once);
+    }
+}
+}
